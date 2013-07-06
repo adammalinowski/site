fc85407f788d056462ee200b230b3a053ae68bac
amalinowski <ad@buntu.(none)>
2013-07-06 02:14:56 +0100

    day night toggle

--- js/js.js
+++ js/js.js
@@ -1,25 +1,63 @@
-var night = function() {  // make it dark
-    document.write('<link rel="stylesheet" type="text/css" href="dark.css">');
-};
-
-var localStorageAvailable = function() {  // thanks modernizr
-    try {
-        localStorage.setItem('k', 'v');
-        localStorage.removeItem('k');
-        return true;
-    } catch(e) {
-        return false;
+// onload?
+// preventdefault?
+(function() {
+    var addEvent = function(elem, name, func) {
+        elem.addEventListener ? elem.addEventListener(name, func) : elem.attachEvent(name, func);
     }
-};
 
-if (localStorageAvailable()) {
-    // figure out current state, defaulting to 'day'
-    var state = localStorage.getItem('dayOrNight');
-    if (state === null || (state != 'day' && state != 'night')) {
-        state = 'day';
-    }
-    // show appropriate toggle
-    // attach handlers
-    night();
-};
+    var localStorageAvailable = function() {
+        try {
+            localStorage.setItem('k', 'v');
+            localStorage.removeItem('k');
+            return true;
+        } catch(e) {
+            return false;
+        }
+    };
 
+    var show = function(elem) { elem.style.display = 'inline'; }
+    var hide = function(elem) { elem.style.display = 'none'; }
+
+    var dayOn = document.getElementById("dayOn");;
+    var nightOn = document.getElementById("nightOn");
+    var dayOff = document.getElementById("dayOff");
+    var nightOff = document.getElementById("nightOff");
+
+    var night = function(e) {
+        var ss = document.createElement("link");
+        ss.type = "text/css";
+        ss.rel = "stylesheet";
+        ss.href = "dark.css";
+        ss.id = "darkstyle";
+        document.getElementsByTagName("head")[0].appendChild(ss);
+        //document.write('<link rel="stylesheet" type="text/css" href="dark.css" id="darkstyle">');
+        show(nightOn);
+        show(showDay);
+        hide(dayOn);
+        hide(showNight);
+        localStorage.setItem('dayOrNight', 'night');
+        if (e) { e.preventDefault() };
+    };
+
+    var day = function(e) {
+        var darkstyle = document.getElementById("darkstyle");
+        if (darkstyle) { darkstyle.parentNode.removeChild(darkstyle); };
+        show(dayOn);
+        show(showNight);
+        hide(showDay);
+        hide(nightOn);
+        localStorage.setItem('dayOrNight', 'day');
+        if (e) { e.preventDefault() };
+    };
+
+    if (localStorageAvailable()) {
+        show(document.getElementById("dayNight"));
+        var state = localStorage.getItem('dayOrNight');
+        if (state === null || (state != 'day' && state != 'night')) {
+            state = 'day';
+        };
+        if (state == 'day') { day(); } else { night(); };
+        addEvent(showNight, 'click', night);
+        addEvent(showDay, 'click', day);
+    };
+})();
